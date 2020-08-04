0d9087d5dae868df8007f25d701274cceb1c48bf
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2020-08-04 15:33:50 +0300

    Make csv parser work

--- task-1.2/src/main.js
+++ task-1.2/src/main.js
@@ -1,1 +1,69 @@
-console.log('main is running');
+import fs from 'fs';
+import path from 'path';
+import readline from 'readline';
+import csvToJson from 'csvtojson';
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+const ask = (question) => new Promise((resolve) => {
+  rl.question(question, answer => resolve(answer));
+});
+
+const dirPath = 'csv';
+let csvFilename = 'nodejs-hw1-ex2.csv';
+let txtFilename = null;
+
+main();
+
+async function main() {
+  const filename = await ask(`Enter file name from "task-1.2/csv" folder (${csvFilename}): `);
+  rl.close();
+  csvFilename = filename.trim() || csvFilename;
+  if (!path.extname(csvFilename)) {
+    csvFilename += '.csv';
+  }
+  txtFilename = path.basename(csvFilename).split(/\.[^.]*$/)[0] + '.txt';
+  const csvPath = path.resolve(path.dirname('.'), dirPath, csvFilename);
+  const txtPath = path.resolve(path.dirname('.'), dirPath, txtFilename);
+
+  const parser = csvToJson().fromFile(csvPath);
+  const output = fs.createWriteStream(txtPath, { flags: 'w' });
+
+  parser.on('data', (buf) => {
+    const source = JSON.parse(buf.toString());
+    const target = Object.keys(source).reduce((acc, key) => {
+      acc[key.toLowerCase()] = getJsonTypeValue(source[key]);
+      return acc;
+    }, {});
+    output.write(JSON.stringify(target) + '\n');
+  });
+
+  parser.on('done', (error) => {
+    if (error) {
+      console.log(error.message);
+      console.log('Execution is interrupted.');
+      return;
+    }
+    if (output.writable) {
+      console.log(`Text file is successfully created: ${txtPath}`);
+      output.end();
+    }
+  });
+
+  output.on('error', (error) => {
+    console.log(error.message);
+    console.log('Execution is interrupted.');
+    parser.end();
+    output.end();
+  });
+}
+
+function getJsonTypeValue(strValue) {
+  if (strValue === 'null') {
+    return null;
+  }
+  if (!isNaN(strValue)) {
+    return +strValue;
+  }
+  return strValue;
+}
