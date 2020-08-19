628ed42f250e8ee3814215f53e79b85e8aadca5f
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2020-08-19 16:22:46 +0300

    Add lint

---
+++ task-2.1/lib/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var _fs = _interopRequireDefault(require("fs"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log('hello');
+var x = 42;
+var y = 525;
+console.log('======== Hello from index.js ========');
