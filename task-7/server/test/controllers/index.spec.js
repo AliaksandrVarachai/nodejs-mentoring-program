462f82468f5e93cae64c32b5c676d3af34dec1d3
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2020-11-27 00:31:05 +0300

    Configure mocha

---
+++ task-7/server/test/controllers/index.spec.js
@@ -0,0 +1,14 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import '../../src/controllers/index.js';
+
+const { expect } = chai;
+
+describe('Controllers', function() {
+  beforeEach(() => {});
+  afterEach(() => {});
+
+  it('Should pass', function() {
+    expect(true).to.be.true;
+  });
+});
